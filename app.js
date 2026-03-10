dc6b1f9712a015e928de3c8d72367c32cdb89f75
kaveri kuwar <kaleshrawani16@gmail.com>
2026-03-10 13:26:07 +0530

    Fix CORS for production domain

--- app.js
+++ app.js
@@ -40,8 +40,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: true,
+  origin: [
+    "https://ykcfinserv.com",
+    "https://www.ykcfinserv.com"
+  ],
   credentials: true,
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
